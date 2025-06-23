8e83f0b0ebfc06678bed650985b824261d5f8965
novailable <97833342+novailable@users.noreply.github.com>
2025-06-23 17:58:18 +0800

    23/ Jun

--- inc/rt1.h
+++ inc/rt1.h
@@ -32,12 +32,35 @@ typedef	struct s_image
 	int		endian;
 }	t_image;
 
+ auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center
+                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+
 typedef	struct s_camera
 {
 	float		vp_width;	// view_port
 	float		vp_height;
 	float		fl;			// focal length
 	t_vector	center;
+	t_vector	vec3_x;		// u
+	t_vector	vec3_y;		// y
+	t_vector	
 	
 }	t_camera;
 
