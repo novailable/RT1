a039eae0585f34eb7cef0e4be19281d22412c200
novailable <97833342+novailable@users.noreply.github.com>
2025-06-26 00:29:20 +0800

    simplified hit_sphere

--- src/components/sphere.c
+++ src/components/sphere.c
@@ -3,17 +3,17 @@
 float	hit_sphere(t_vector	sp_center, float radius, t_ray r)
 {
 	t_vector	oc;
-	float		abc[3];
+	float		ahc[3];
 	float		delta;
 
 	oc = vec3_sub(sp_center, r.origin);
-	abc[0] = vec3_dot(r.dir, r.dir);
-	abc[1] = -2.0 * vec3_dot(r.dir, oc);
-	abc[2] = vec3_dot(oc, oc) - radius * radius;
-	delta = abc[1] * abc[1] - 4 * abc[0] * abc[2];
+	ahc[0] = vec3_len_sq(r.dir);
+	ahc[1] = vec3_dot(r.dir, oc);
+	ahc[2] = vec3_len_sq(oc) - radius * radius;
+	delta = ahc[1] * ahc[1] - ahc[0] * ahc[2];
 
 	if (delta < 0)
 		return (-1.0);
 	else
-		return ((- abc[1] - sqrt(delta)) / (2.0 * abc[0]));
+		return ((ahc[1] - sqrt(delta)) / ahc[0]);
 }
